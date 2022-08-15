57a362850af383257be1fc18f1ff6570bd6c91fe
ailtonbsj <ailton.ifce@gmail.com>
2022-08-15 11:11:43 -0300

    Added epson-l355

--- compile.sh
+++ compile.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-dpkg-deb -b epson-l3110 .
-dpkg-deb -b epson-l3150 .
 dpkg-deb -b epson-l3250 .
-dpkg-deb -b epson-l396 .
-dpkg-deb -b epson-l375 .
-dpkg-deb -b epson-l380 .
-dpkg-deb -b epson-l210 .
+dpkg-deb -b epson-l3150 .
+dpkg-deb -b epson-l3110 .
 dpkg-deb -b epson-l555 .
+dpkg-deb -b epson-l396 .
+dpkg-deb -b epson-l380 .
+dpkg-deb -b epson-l375 .
+dpkg-deb -b epson-l355 .
+dpkg-deb -b epson-l210 .
+
 dpkg-deb -b lexmark-mx722 .
 dpkg-deb -b lubuntu-openvpn .
 dpkg-deb -b oracle-jdk-17-lts .
